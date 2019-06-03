4bb301aca4aebb2245f199ef429ecbcaba96ce90
dfSabogal10 <david.sabogal1@gmail.com>
2019-06-03 17:25:59 -0500

    heroku deploy

--- herramienta/forms.py
+++ herramienta/forms.py
@@ -2,15 +2,15 @@ from django import forms
 from herramienta import models
 
 class HerramientasForm(forms.Form):
-    #periodosMedicion = models.InstrumentoMedicion.objects.all().values_list('periodo', flat=True).distinct().order_by('periodo')
+    periodosMedicion = models.InstrumentoMedicion.objects.all().values_list('periodo', flat=True).distinct().order_by('periodo')
     CHOICES=[]
-    #for i,p in enumerate(periodosMedicion):
-    #    CHOICES.append((p,p))
+    for i,p in enumerate(periodosMedicion):
+        CHOICES.append((p,p))
     periodo = forms.ChoiceField(label='Escoge el periodo del que deseas ver las herramientas de medicion:',widget=forms.Select(attrs={'class' : 'form-control'}),choices=CHOICES)
 
 class PlanesForm(forms.Form):
-    #periodosPlanes = models.PlanesDeMejora.objects.all().values_list('periodo', flat=True).distinct().order_by('periodo')
+    periodosPlanes = models.PlanesDeMejora.objects.all().values_list('periodo', flat=True).distinct().order_by('periodo')
     CHOICES=[]
-    #for i,p in enumerate(periodosPlanes):
-    #    CHOICES.append((p,p))
+    for i,p in enumerate(periodosPlanes):
+        CHOICES.append((p,p))
     periodo = forms.ChoiceField(label='Escoge el periodo del que deseas ver los planes de medicion:',widget=forms.Select(attrs={'class' : 'form-control'}),choices=CHOICES)
